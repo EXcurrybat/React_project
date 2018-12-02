f332dcc36763fb25f00df11ff852c90bf9a19188
boweichn <boweicheng@hotmail.com>
2018-12-01 16:37:39 -0800

    working ammo count, timer, and life display

--- components/GameScreen.js
+++ components/GameScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
 
 export default class GameScreen extends React.Component {
   constructor(props){
@@ -9,31 +9,66 @@ export default class GameScreen extends React.Component {
       blaster: require('../assets/spriteAssets/blaster.gif'),
       initialShipY: 0,
       initialShipX: 180,
-      blasterOriginX: 0,
-      blasterOriginY: 0,
-      displayBlaster: 0,
-      
+      blaster1OriginX: 0,
+      blaster1OriginY: 445,
+      blaster2OriginX: 25,
+      blaster2OriginY: 445,
+      blaster3OriginX: 50,
+      blaster3OriginY: 445,
+      ammoCount:3,
+      gameTime:0,
+      bulletFrame:0,
+      lifeCount: 3     
     }
+  }
+
+  resetDirection = () => {
+    this.props.direction.buttonDirection = ''
+  }
+
+  resetAB = () => {
+    this.props.direction.abInputs = ''
+    this.props.direction.buttonDirection = ''
+  }
+
+  componentDidMount() {
+    this.aniTime = setInterval(() => {
+      this.setState({gameTime: this.state.gameTime+1})
+    }, 1000);
   }
 
   render() {
     if (this.props.direction.buttonDirection == 'Up' && this.state.initialShipY < 400) {
       this.setState({initialShipY: this.state.initialShipY+=10})
-      this.props.direction.buttonDirection = ''
+      this.resetDirection()
     } else if (this.props.direction.buttonDirection == 'Down' && this.state.initialShipY > 0) {
       this.setState({initialShipY: this.state.initialShipY-=10})
-      this.props.direction.buttonDirection = ''
+      this.resetDirection()
     } else if (this.props.direction.buttonDirection == 'Left' && this.state.initialShipX > 0) {
       this.setState({initialShipX: this.state.initialShipX-=10})
-      this.props.direction.buttonDirection = ''
+      this.resetDirection()
     } else if (this.props.direction.buttonDirection == 'Right' && this.state.initialShipX < 350) {
       this.setState({initialShipX: this.state.initialShipX+=10})
-      this.props.direction.buttonDirection = ''
+      this.resetDirection()
     }
 
     return (
       <View style={styles.gameScreen}>
         <Image source={this.state.shipImage} style={{bottom:this.state.initialShipY, left: this.state.initialShipX, position: 'absolute', width: 50, height: 50}}/>
+        <Text style={{color: 'white', position: 'absolute', left:7, top:0}}>Ammo Count:</Text>
+        <Text style={{color: 'white', position: 'absolute', top:0}}>Game Time:</Text>
+        <Text style={{color: 'white', position: 'absolute', top:25}}>{this.state.gameTime}</Text>
+        <Text style={{color: 'white', position: 'absolute', top:0, right:25}}>Life Count: </Text>
+        <Text style={{color: 'white', position: 'absolute', top:25, right:70}}>{this.state.lifeCount}</Text>
+        <View style={{bottom:this.state.blaster1OriginY, left: this.state.blaster1OriginX, position: 'absolute', width: 50, height: 50}}>
+          <Image source={this.state.blaster} style={{height: 50, width: 50}}/>
+        </View>
+        <View style={{bottom:this.state.blaster2OriginY, left: this.state.blaster2OriginX, position: 'absolute', width: 50, height: 50}}>
+          <Image source={this.state.blaster} style={{height: 50, width: 50}}/>
+        </View>
+        <View style={{bottom:this.state.blaster3OriginY, left: this.state.blaster3OriginX, position: 'absolute', width: 50, height: 50}}>
+          <Image source={this.state.blaster} style={{height: 50, width: 50}}/>
+        </View>
         {this.shootBlaster()}
       </View>
     );
@@ -41,11 +76,22 @@ export default class GameScreen extends React.Component {
 
   shootBlaster = () => {
     if (this.props.direction.abInputs == 'A') {
-      this.setState({blasterOriginX: this.state.initialShipX, blasterOriginY: this.state.initialShipY+15})
-      this.props.direction.abInputs = ''
-      this.props.direction.buttonDirection = ''
-      console.log('pressed')
-      return <Image source={this.state.blaster} style={{bottom:this.state.blasterOriginY, left: this.state.blasterOriginX, position: 'absolute', width: 50, height: 50}}/>
+      if (this.state.ammoCount == 1){
+        this.setState({blaster1OriginX: this.state.initialShipX, blaster1OriginY: this.state.initialShipY+15, ammoCount: this.state.ammoCount-1})
+        this.resetAB()
+        console.log('pressed ammo1')
+      } else if (this.state.ammoCount == 2){
+        this.setState({blaster2OriginX: this.state.initialShipX, blaster2OriginY: this.state.initialShipY+15, ammoCount: this.state.ammoCount-1})
+        this.resetAB()
+        console.log('pressed ammo2')
+      } else if (this.state.ammoCount == 3){
+        this.setState({blaster3OriginX: this.state.initialShipX, blaster3OriginY: this.state.initialShipY+15, ammoCount: this.state.ammoCount-1})
+        this.resetAB()
+        console.log('pressed ammo3')
+      }
+    } if (this.props.direction.abInputs == 'B') {
+      this.setState({blaster1OriginX: 0, blaster1OriginY: 445, blaster2OriginX: 25, blaster2OriginY: 445, blaster3OriginX: 50, blaster3OriginY: 445, ammoCount:3})
+      this.resetAB()
     }
   }
 }
