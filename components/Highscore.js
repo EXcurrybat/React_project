debdd63632b14d3be6b48f689935f0e8b2edca39
boweichn <boweicheng@hotmail.com>
2018-12-03 20:33:07 -0800

    highscore layout complete

--- components/Highscore.js
+++ components/Highscore.js
@@ -19,7 +19,7 @@ export default class HighscoreComponent extends Component {
 				style = {styles.button}
 				underlayColor = "white"
 			>
-				<Text style={styles.title}>Score: {task.item.score}</Text>
+				<Text style={styles.title}>Score: {task.item.score} pts</Text>
 			</TouchableHighlight>
 		)
 	}
@@ -27,12 +27,32 @@ export default class HighscoreComponent extends Component {
 	goBack(){
 		console.log("go back");
 	}
+	
+	componentDidMount(){
+		var highscores = require('../highscore.json');
+		var topFive = [];
+		for(var index= 0; index < highscores.highscores.length && index < 5; index++){
+			topFive.push(highscores.highscores[index]);
+		}
+		
+		this.setState({
+			items: topFive
+		})
+		
+	}
     
 
     render() {
         return (
 			<View style={styles.gameScreen}>
-				<Text style={{color: 'white'}}>This is Score</Text>
+				<Text style={styles.mainTitle}>Highscores</Text>
+				<View style={styles.listContainer}>
+					<FlatList
+						data = {this.state.items}
+						renderItem = {this.renderRow}
+						keyExtractor={(item, index) => index.toString()}
+					/>
+				</View>
 			</View>
         )
     }
@@ -46,5 +66,39 @@ const styles = StyleSheet.create({
         height: '100%', width: '100%',
         backgroundColor: 'black',
         justifyContent:'center', alignItems:'center'
-    }
+    },
+	mainTitle: {
+        position: 'absolute',
+        top: 0,
+        margin: 'auto',
+        color: 'white',
+        fontWeight: "900",
+        fontSize: 72,
+        textShadowColor: 'rgba(150, 150, 150, 0.75)',
+        textShadowOffset: {width: -2, height: 2},
+        textShadowRadius: 12
+    },
+	listContainer:{
+		position: 'absolute',
+		top: 100,
+		width: '100%'
+	},
+	button: {
+		height: 40,
+		flexDirection: 'row',
+		backgroundColor: 'white',
+		borderColor: 'black',
+		borderWidth: 3,
+		borderRadius: 8,
+		marginBottom: 5,
+		marginTop: 5,
+		width: '100%',
+		height: 45,
+		alignSelf: 'stretch',
+		justifyContent: 'center'
+	},
+	title: {
+		fontSize: 25,
+		textAlign: 'center'
+	}
 })
